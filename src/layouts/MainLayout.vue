1a97768a4397c3f7873a5de4fb0f0ed7f0f6f6c4
Molly Huang <molly.h@gmail.com>
2021-04-24 15:46:28 +0800

    #25. Desktop - Constrain Content for Wider Screens

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header class="bg-white text-grey-10" bordered>
-      <q-toolbar>
+      <q-toolbar class="constrain">
         <q-btn
           to="/camera"
           class="large-screen-only q-mr-sm"
