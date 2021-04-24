0ff7cacecb46a126854378bd3d685c73f9c37415
Molly Huang <molly.h@gmail.com>
2021-04-24 16:17:30 +0800

    29. Create a List of Posts - Post Header

--- src/pages/pageHome.vue
+++ src/pages/pageHome.vue
@@ -1,6 +1,23 @@
 <template>
   <q-page class="constrain q-pa-md">
-    <h5>Home Page</h5>
+    <q-card class="card-post" flat bordered>
+      
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://instagram.ftpe7-1.fna.fbcdn.net/v/t51.2885-19/11373805_1672166586350116_972886693_a.jpg?_nc_ht=instagram.ftpe7-1.fna.fbcdn.net&_nc_ohc=TuNXbIEO_RkAX8v5P6P&edm=ABfd0MgAAAAA&ccb=7-4&oh=3784e2412487c770a7e760f1c79ecb03&oe=60AA8037&_nc_sid=7bff83">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-bold">Molly Huang</q-item-label>
+          <q-item-label caption>Taipei, Taiwan</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+
+    </q-card>
   </q-page>
 </template>
 
